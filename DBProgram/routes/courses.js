08c11b9362ec1d4210bd3a5671c2bc16cfad846a
Zak Wright <zakawright#gmail.com>
2016-01-08 13:49:17 -0600

    Still working on it. Not 100% Steamy

--- DBProgram/routes/courses.js
+++ DBProgram/routes/courses.js
@@ -16,5 +16,49 @@ router.get('/', function(req, res, next) {
     res.render('entities/courses/index', { title: 'courses', data: rows });
   });
 });
+/* GET form - create course */
+router.get('/create', function(req,res,next) {
+  res.render('entities/courses/create', { title: 'New Course' });
+});
+
+router.post('/create', function(req,res,next){
+  var data = req.body;
+  res.send(req.body)
+  // Begin a transaction.
+  // http://stackoverflow.com/questions/28803520/does-sqlite3-have-prepared-statements-in-node-js
+  db.beginTransaction(function(err, transaction) {
+    // Now we are inside a transaction.
+    // Use transaction as normal sqlite3.Database object.
+    transaction.run(
+      "INSERT INTO Course(TeacherID,CourseName,TimeHeld) " +
+      "VALUES(?,?,?)",
+      data.TeacherID,
+      data.CourseName,
+      data.TimeHeld
+    );
+
+
+    transaction.commit(function(err) {
+      if(err)
+        console.log('commit fail');
+      else
+        console.log('commit success');
+    });
+  });
+
+  res.send(req.body);
+});
+
+/* DELETE -  delete course */
+var courseID
+router.delete('/delete',function(req,res,next){
+  res.send(req.body)
+  db.beginTransaction(function(err,transaction){
+    transacion.run (
+      "DELETE FROM Courses" +
+      "WHERE CourseID = "
+    )
+  });
+});
 
 module.exports = router;
