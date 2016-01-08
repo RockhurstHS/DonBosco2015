bceecfa4e44075f6b53cb995ca648d7e6c5f49c8
Daniel Henry <danielhenry1@Daniel-Henrys-MacBook-Pro.local>
2016-01-08 13:47:26 -0600

    Next level baby

--- DBProgram/routes/teachers.js
+++ DBProgram/routes/teachers.js
@@ -9,7 +9,7 @@ var db = new TransactionDatabase(engine);
 
 engine.exec("PRAGMA foreign_keys = ON");
 
-/* GET all dealerships home page. */
+/* GET all teachers home page. */
 router.get('/', function(req, res, next) {
   db.all("SELECT * FROM teacher", function(err,rows){
     console.log('teachers rows fetched: ' + rows.length);
@@ -24,7 +24,6 @@ router.get('/create', function(req,res,next) {
 /* POST new teacher */
 router.post('/create', function(req,res,next){
   var data = req.body;
-  res.send(req.body)
   // Begin a transaction.
   // http://stackoverflow.com/questions/28803520/does-sqlite3-have-prepared-statements-in-node-js
   db.beginTransaction(function(err, transaction) {
@@ -49,8 +48,75 @@ router.post('/create', function(req,res,next){
         console.log('commit success');
     });
   });
+res.redirect('/teachers');
+
+});
+
+/* GET form - delete teacher */
+router.get('/delete', function(req,res,next) {
+  res.render('entities/teachers/delete', { title: 'Delete Teacher' });
+});
+
+/* DELETE new teacher */
+router.post('/delete', function(req,res,next){
+  var data = req.body;
+  // Begin a transaction.
+  // http://stackoverflow.com/questions/28803520/does-sqlite3-have-prepared-statements-in-node-js
+  db.beginTransaction(function(err, transaction) {
+    // Now we are inside a transaction.
+    // Use transaction as normal sqlite3.Database object.
+    transaction.run(
+     "DELETE FROM Teacher WHERE LastName=?",
+      data.LastName
+      );
+
+
+    transaction.commit(function(err) {
+      if(err)
+        console.log('commit fail');
+      else
+        console.log('commit success');
+
+    });
+res.redirect('/teachers');
+  });
+
+});
+
+/* GET form - update teacher */
+router.get('/update', function(req,res,next) {
+  res.render('entities/teachers/update', { title: 'Update Teacher' });
+});
+
+/* POST update teacher */
+router.post('/update', function(req,res,next){
+  var data = req.body;
+  res.send(req.body)
+  // Begin a transaction.
+  // http://stackoverflow.com/questions/28803520/does-sqlite3-have-prepared-statements-in-node-js
+  db.beginTransaction(function(err, transaction) {
+    // Now we are inside a transaction.
+    // Use transaction as normal sqlite3.Database object.
+    transaction.run(
+      "UPDATE Teacher SET FirstName=?, MiddleInitial=?, LastName=?, Email=?, PhoneNumber=?, Gender=? WHERE ID=?",
+      data.FirstName,
+      data.MiddleInitial,
+      data.LastName,
+      data.Email,
+      data.PhoneNumber,
+      data.Gender,
+      data.ID
+    );
+
+
+    transaction.commit(function(err) {
+      if(err)
+        console.log('commit fail');
+      else
+        console.log('commit success');
+    });
+  });
 
   res.send(req.body);
 });
-
 module.exports = router;
