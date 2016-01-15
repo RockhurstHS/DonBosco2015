ca71031342a56c7f5997c3954b302a1a64f99929
Sam Wilkerson <samwilkerson@Catherines-MacBook-Air-2.local>
2016-01-15 13:28:04 -0600

    database updates

--- DBProgram/NewDatabase1.sql
+++ DBProgram/NewDatabase1.sql
@@ -70,6 +70,174 @@ FOREIGN KEY (StudentID) REFERENCES Student(ID),
 FOREIGN KEY (CourseID) REFERENCES Course(ID)
 );
 
+ CREATE TABLE Reading(
+  TestNumber                         INTEGER,
+  TestLetter                         VARCHAR(1),
+  NumbersAndNumeracyTerms            TEXT,
+  MorphemesAndSyntax                 TEXT,
+  WordsAndPhrases                    TEXT,
+  SameMeaning                        TEXT,
+  OppositeMeaning                    TEXT,
+  AppropriateAssociatedInformation   TEXT,
+  IdentifyInformation                TEXT,
+  ConstructMeaning                   TEXT,
+  ExtendMeaning                      TEXT,
+);
+
+INSERT INTO Reading(TestNumber, TestLetter, NumbersAndNumeracyTerms, MorphemesAndSyntax, WordsAndPhrases, SameMeaning, OppositeMeaning, AppropriateAssociatedInformation, IdentifyInformation, ConstructMeaning, ExtendMeaning)
+VALUES(1, 'A', '1,3,5,8', '', '2,4,6,7,9', '', '', '13,14,17', '10,11,12,15,18,19,20,21,23,25', '16,22,24',''),
+(1, 'B', '2,3,10,12', '', '1,4,5,6,7,8', '', '', '16,22', '13,14,15,18,19,20,21,23,24,25', '9,11,17', ''),
+(2, 'A', '3,4,8,9,12,18,22', '10', '6,17,21', '11', '', '25', '1,2,5,14,15,16,19,20', '7,13,23,24', ''),
+(2, 'B', '1,7,10,22', '20', '2,24,25', '15,21', '', '8,11,12,19', '3,4,5,9,13,14,17,23', '6,16,18', ''),
+(3, 'A', '4,9,14,15', '11,13,21', '8,19', '3,20', '25', '10', '1,6,12,22', '5,7,16,23,24', '2,17,18'),
+(3, 'B', '6,11,16,17', '15,22,25', '4', '9,21', '5', '13,20', '1,7,14,18', '2,3,8,23,24', '10,12,19'),
+(4, 'A', '5,8,12,16', '1,9,21', '20,22', '15,19', '', '4,6,14', '2,3,10,13,17,23', '11,18,24,25', '7'),
+(4, 'B', '3,4,8,11', '18', '1,21,25', '14,15,23', '19', '7,17', '9,20,24', '6,10,13,16', '2,5,12,22');
+
+CREATE TABLE Listening(
+  TestNumber INTEGER,
+  TestLetter VARCHAR(1),
+  NumberandNumberTerms TEXT,
+  DiscernSounds TEXT,
+  Details TEXT,
+  StatedConcepts TEXT,
+  IdiomsExpressions TEXT,
+  DetermineRoles TEXT,
+  Instructions TEXT,
+  CauseEffect TEXT,
+  FactOpinion TEXT,
+  MainIdea TEXT,
+  Forecast TEXT,
+  SpeakerPurpose TEXT
+);
+
+INSERT INTO Listening(TestNumber, TestLetter, NumberandNumberTerms, DiscernSounds, Details, StatedConcepts, IdiomsExpressions, DetermineRoles, Instructions, CauseEffect, FactOpinion, MainIdea, Forecast, SpeakerPurpose)
+VALUES(1, 'A', '1,2,3,5', '8,9,10', '14,17,20', '11,23', '21', '4,18', '6,7', '24', '', '12,15,25', '13,16', '19,22'),
+(1, 'B', '3,4,20,23', '13,14,15,16', '22', '17', '5,10', '6,19', '1,2,7', '18,25', '', '21,24', '9', '8,11,12'),
+(2, 'A', '1,2,3,4,5', '10,11,12', '17', '', '18,20', '6,7,16,21', '8,9', '13,23', '', '14,24', '19,22', '15,25'),
+(2, 'B', '1,4,5,19', '10,11,12', '20,24', '23', '7,16', '2,14,17', '3,15', '9', '', '18,21,25', '6,8,22', '13'),
+(3, 'A', '1,2,3,4', '', '9,15,19,23', '12,16', '20,21', '7', '5,6,8', '10', '17,24', '11,13,25', '22', '14,18'),
+(3, 'B', '1,2,3,4', '', '7,14,15,19,23', '18', '10,17,24', '5,6', '', '11,12', '20', '13,16,22', '8,25', '9,21'),
+(4, 'A', '1,2,3,4', '', '11,16,19,22', '12,17,23', '9,13,18,24', '5,6', '7,8', '', '20', '10,14', '21', '15,25'),
+(4, 'B', '1,2,3,16', '', '14,15,22,25', '13,20,24', '21', '4,5', '6,17', '8', '9,10', '12,18,23', '7,11', '19');
+
+
+
+CREATE TABLE Writing(
+    TestNumber INTEGER,
+    TestLetter VARCHAR(1),
+    NumbersandNumercay      TEXT,
+    VerbPhrases             TEXT,
+    NounPhrases             TEXT,
+    WritingConventions      TEXT,
+    SentenceFormation       TEXT,
+    SentenceCoherence       TEXT,
+    WriteToDescribe         TEXT,
+    WriteToAcomplishTasks   TEXT,
+    ExtendedWriting         TEXT
+);
+
+
+INSERT INTO Writing(TestNumber,TestLetter,NumbersandNumercay,VerbPhrases,
+    NounPhrases,WritingConventions,SentenceFormation,SentenceCoherence,
+    WriteToDescribe,WriteToAcomplishTasks,ExtendedWriting)
+VALUES (1,'A','1,2,3,5','4,8,11,12','6,7,9,16','10,13,14,15','17,18',
+        '19,20','1,2','3,4','5');
+INSERT INTO Writing(TestNumber,TestLetter,NumbersandNumercay,VerbPhrases,
+    NounPhrases,WritingConventions,SentenceFormation,SentenceCoherence,
+    WriteToDescribe,WriteToAcomplishTasks,ExtendedWriting)
+VALUES (1,'B','1,2,3,4','5,8,16,17','6,7,9,15','10,12,13,14','11,18',
+        '19,20','1,2','3,4','5');
+INSERT INTO Writing(TestNumber,TestLetter,NumbersandNumercay,VerbPhrases,
+    NounPhrases,WritingConventions,SentenceFormation,SentenceCoherence,
+    WriteToDescribe,WriteToAcomplishTasks,ExtendedWriting)
+VALUES (2,'A','1,2,3,4','5,6,7,10,14','8,9,11','12,13,15','16,17',
+        '18,19,20','1,2','3,4','5');
+INSERT INTO Writing(TestNumber,TestLetter,NumbersandNumercay,VerbPhrases,
+    NounPhrases,WritingConventions,SentenceFormation,SentenceCoherence,
+    WriteToDescribe,WriteToAcomplishTasks,ExtendedWriting)
+VALUES (2,'B','1,2,3,4','5,6,8,13,','7,9,12,14,16','11,15',
+        '10,17','18,19,20','1,2','3,4','5');
+INSERT INTO Writing(TestNumber,TestLetter,NumbersandNumercay,VerbPhrases,
+    NounPhrases,WritingConventions,SentenceFormation,SentenceCoherence,
+    WriteToDescribe,WriteToAcomplishTasks,ExtendedWriting)
+VALUES (3,'A','1,2,3,7','8,11,13,14','4,5,6','9,10,15','12,16','17,18,19,20',
+        '1,2','3,4','5');
+INSERT INTO Writing(TestNumber,TestLetter,NumbersandNumercay,VerbPhrases,
+    NounPhrases,WritingConventions,SentenceFormation,SentenceCoherence,
+    WriteToDescribe,WriteToAcomplishTasks,ExtendedWriting)
+VALUES (3,'B','1,2,3,4','5,6,7','8,9,10,11','14,15','13,16','12,17,19,19,20'
+        '1,2','3,4','5');
+INSERT INTO Writing(TestNumber,TestLetter,NumbersandNumercay,VerbPhrases,
+    NounPhrases,WritingConventions,SentenceFormation,SentenceCoherence,
+    WriteToDescribe,WriteToAcomplishTasks,ExtendedWriting)
+VALUES (4,'A','1,2,3,4','7,8,9,13','5,6,15','10,11,12','14,16','17,18,19,20',
+        '1,2','3,4','5');
+INSERT INTO Writing(TestNumber,TestLetter,NumbersandNumercay,VerbPhrases,
+    NounPhrases,WritingConventions,SentenceFormation,SentenceCoherence,
+    WriteToDescribe,WriteToAcomplishTasks,ExtendedWriting)
+VALUES (4,'B','1,2,3,4','5,9,10,11','7,8,14,16','6,13','12,15','17,18,19,20'
+        '1,2','3,4','5');
+
+CREATE TABLE Tests(
+  StudentID INTEGER,
+  Subject TEXT,
+  TestNumber INTEGER,
+  TestLetter VARCHAR(1),
+  P1 BOOLEAN,
+  P2 BOOLEAN,
+  P3 BOOLEAN,
+  P4 BOOLEAN,
+  P5 BOOLEAN,
+  P6 BOOLEAN,
+  P7 BOOLEAN,
+  P8 BOOLEAN,
+  P9 BOOLEAN,
+  P10 BOOLEAN,
+  P11 BOOLEAN,
+  P12 BOOLEAN,
+  P13 BOOLEAN,
+  P14 BOOLEAN,
+  P15 BOOLEAN,
+  P16 BOOLEAN,
+  P17 BOOLEAN,
+  P18 BOOLEAN,
+  P19 BOOLEAN,
+  P20 BOOLEAN,
+  P21 BOOLEAN,
+  P22 BOOLEAN,
+  P23 BOOLEAN,
+  P24 BOOLEAN,
+  P25 BOOLEAN
+);
+
+Create Table Speaking(
+  TestNumber                            INTEGER,
+  TestLetter                            VARCHAR(1),
+  NumbersAndNumeracyTerms               TEXT,
+  GreetingsInquiriesLeaveTakings        TEXT,
+  PhoneCompetency                       TEXT,
+  ExpressOpinion                        TEXT,
+  InstructionsDirectionsLocations       TEXT,
+  Descriptions                          TEXT,
+  NarrateParaphraseEvents               TEXT,
+  RequestsPermission                    TEXT,
+  ExpressDissatifactionApology          TEXT,
+  NegotiateMeaning                      TEXT
+);
+INSERT INTO Speaking(TestNumber,TestLetter,NumbersAndNumeracyTerms,GreetingsInquiriesLeaveTakings,PhoneCompetency,ExpressOpinion,
+      InstructionsDirectionsLocations,Descriptions,NarrateParaphraseEvents,RequestsPermission,ExpressDissatificationApology
+      ,NegotiateMeaning)
+Values (1, 'A', '1,2,3', '9,10', '11', '', '4,5,6', '', '7,8', '12,13,14', '15', '16'),
+(1, 'B', '1,2,3', '9,10', '11', '', '4,5,6', '', '7,8', '12,13,14', '15', '16'),
+(2, 'A', '1,2,3', '9', '10', '11', '4,5,6', '', '7,8', '12,13,14', '15', '16'),
+(2, 'B', '1,2,3', '9', '10', '11', '4,5,6', '', '7,8', '12,13,14', '15', '16'),
+(3, 'A', '1,2,3', '', '9,10', '11', '4,5', '', '6,7,8', '12,13', '14,15', '16'),
+(3, 'B', '1,2,3', '', '9,10', '12', '4,5', '', '6,7,8', '11,13', '14,15', '16'),
+(4, 'A', '1,2,3', '', '9,10', '7,8', '4,5', '6', '', '11,12', '13,15', '14,16'),
+(4, 'B', '1,2,3', '', '9,10' '7,8', '4,5', '6', '', '11,12', '13,14', '15,16');
+
+
 
 /*
 Begining of values
